bfeeacdb2a9382b14d3b9a84da3d201a99f04eb4
Igniske <104842998+Igniske@users.noreply.github.com>
2023-05-12 10:04:21 -0300

    css fixes

--- PARADA WEB/src/components/Contact.tsx
+++ PARADA WEB/src/components/Contact.tsx
@@ -3,11 +3,11 @@
 function Contact(){
     return(
         <div className="bg-blue-100">
-        <div id="contact" className="hover:bg-blue-600 h-16 lg:h-12 w-full lg:w-5/6 mx-auto rounded-b-none rounded-2xl bg-blue-800">
+        <div id="contact" className="hover:bg-blue-500 h-16 lg:h-12 w-full lg:w-5/6 mx-auto rounded-b-none rounded-2xl bg-blue-800">
         <ul className="flex flex-nowrap justify-around">
-            <li className="text-base lg:text-xl hover:text-white hover:text-2xl"><a target="" href="https://mail.google.com/mail/u/0/?fs=1&to=ignacio.manuel07@gmail.com&su=Contacto&bcc=ignacio.manuel07@gmail.com&tf=cm">Envianos un correo!</a></li>
-            <li className="text-base lg:text-xl hover:text-white hover:text-2xl">Contactanos por telefono!</li>
-            <li className="text-base lg:text-xl hover:text-white my-auto hover:text-2xl"><a target="_blank" href="https://www.google.com/maps/place/Gran+Canaria+1300,+Quilmes,+Provincia+de+Buenos+Aires/@-34.7246398,-58.2646992,17z/data=!3m1!4b1!4m5!3m4!1s0x95a32e6ba254461f:0xbbffbd03ee157d27!8m2!3d-34.7246442!4d-58.2621243?hl=es">Visitanos!</a></li>
+            <li className="text-base lg:text-xl hover:text-white px-4"><a target="" href="https://mail.google.com/mail/u/0/?fs=1&to=ignacio.manuel07@gmail.com&su=Contacto&bcc=ignacio.manuel07@gmail.com&tf=cm">Envianos un correo!</a></li>
+            <li className="text-base lg:text-xl hover:text-white px-4">Contactanos por telefono!</li>
+            <li className="text-base lg:text-xl hover:text-white my-auto px-4 "><a target="_blank" href="https://www.google.com/maps/place/Gran+Canaria+1300,+Quilmes,+Provincia+de+Buenos+Aires/@-34.7246398,-58.2646992,17z/data=!3m1!4b1!4m5!3m4!1s0x95a32e6ba254461f:0xbbffbd03ee157d27!8m2!3d-34.7246442!4d-58.2621243?hl=es">Visitanos!</a></li>
         </ul>
         </div>
         </div>
--- PARADA WEB/src/components/Landing.tsx
+++ PARADA WEB/src/components/Landing.tsx
@@ -3,11 +3,11 @@ function Landing(){
     return <div>
     <div className="h-screen relative">
       <div className="absolute inset-0 background-image opacity-50"></div>
-      <div className="w-1/2 pt-16 m-auto relative z-10">
-        <p className=" lg:pt-36 text-black text-3xl">
+      <div className="lg:w-1/2 w-3/4 pt-16 m-auto relative z-10">
+        <p className=" pt-36 text-black lg:text-3xl text-xl">
           ¡Descubre el placer de la lectura en papel! Sumérgete en el mundo de las noticias y las historias cautivadoras con nuestra amplia selección de diarios y revistas. ¡Encuentra tu próxima aventura entre páginas impresas y mantente al día con lo que más te apasiona!
         </p>
-        <div className="m-auto pt-16 w-1/6">
+        <div className="m-auto pt-16 lg:w-1/6">
             <button className="rounded-full border-2 border-black h-12 w-fit px-4 text-white text-lg bg-blue-600 hover:bg-blue-800 hover:border-white"><a href="#contact">Contacto</a></button>
         </div>
         
